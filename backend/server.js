61d5e0c35d16a72adfdaf54ab8460b9f24dcade2
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-21 13:21:17 +0530

    add netlify link

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://matchmatee.netlify.app"
+    "https://matchmate-8yop.onrender.com"
   ],
   credentials: true
 }));
@@ -21,7 +21,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://matchmatee.netlify.app"
+      "https://matchmate-8yop.onrender.com"
     ],
     methods: ["GET", "POST"],
     credentials: true
